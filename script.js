8676533ca01ae4912321ec5fb8d9f3c92d548585
Alphaalimamy <alphaalimamykamara@gmail.com>
2021-07-11 18:07:11 +0800

    all files

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#toggle-btn").click(function () {
+    $("#navitems").slideToggle(3000);
+  });
+});
+
+// $(document).ready(function () {
+//   $("#toggle-btn").click(function () {
+//     $(".nav-links").slideToggle(3000);
+//   });
+// });
